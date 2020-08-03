f21612f7e38a211fd8c4103a2dbcf921f416ed01
Weee <meowtit58@Gmail.com>
2020-08-03 22:34:40 +0100

    Parse compressed line into a block

--- BlockParser.py
+++ BlockParser.py
@@ -1,4 +1,4 @@
-
+from LineParser import ParseToken,line
 import re
 BlockMappings  = {"BYPASSCF":"BlockBypassCF" ,
              "SOLVECAPTCHA":"BlockSolveCaptcha" ,
@@ -16,8 +16,24 @@ BlockMappings  = {"BYPASSCF":"BlockBypassCF" ,
              "EXECUTEJS":"SBlockExecuteJS" ,
              "NAVIGATE":"SBlockNavigate"}
 
-
-
+from BlockParse import BlockParse
+from BlockRequest import BlockRequest
+from BlockKeycheck import BlockKeycheck
+BlockMappings2  = {"BYPASSCF": None,
+            "SOLVECAPTCHA": None,
+            "REPORTCAPTCHA": None,
+            "CAPTCHA": None,
+            "FUNCTION": None,
+            "KEYCHECK": BlockKeycheck().FromLS,
+            "PARSE":BlockParse().FromLS ,
+            "RECAPTCHA": None,
+            "REQUEST": BlockRequest().FromLS,
+            "TCP": None,
+            "UTILITY": None,
+            "BROWSERACTION": None,
+            "ELEMENTACTION": None,
+            "EXECUTEJS": None,
+            "NAVIGATE": None}
 def GetBlockType(line):
     try:
         return re.match('^!?(#[^ ]* )?([^ ]*)',line).group(2) 
@@ -30,3 +46,26 @@ def IsBlock(line) -> bool:
         return False
     else:
         return True
+
+def Parse(input_line):
+    input_line = input_line.strip()
+    line.current = input_line
+
+    disabled = input_line.startswith("!")
+    if disabled: line.current[1:]
+
+    label = ParseToken(line.current,"Label", False, True)
+    
+    identifier = ""
+    identifier = ParseToken(line.current,"Parameter", True, True)
+
+    block = BlockMappings2.get(identifier)
+    #Todo blocks
+    if block:
+        block = block(line.current)
+        block["label"] = label
+        block["block_type"] = identifier
+        return block
+    else:
+        return None
+    
