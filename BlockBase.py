ff58deccf3f6759a9691b4d38af39c1b89653c2d
Weee <meowtit58@Gmail.com>
2020-08-05 03:08:06 +0100

    ReplaceValuesRecursive

--- BlockBase.py
+++ BlockBase.py
@@ -6,9 +6,11 @@ def ParseArguments(input_string, delimL, delimR):
     for match in matches:
         output.append(match)
     return output
+
 def ReplaceValues(input_string, bot_data):
-    """ Example: bot_data = {"Variables": {"SOURCE":{"Value":{"name":"testes"},
-                                                     "Type": "Dictionary"}},
+    """ Example: bot_data = {"Variables": {"SOURCE":{"Value": ["wadaw"],
+                                                     "Type": "List",
+                                                     "Name": "SOURCE"}},
                              "GlobalVariables": {}}"""
 
     if "<" not in input_string and ">" not in input_string: return input_string
@@ -21,6 +23,7 @@ def ReplaceValues(input_string, bot_data):
         r = re.compile('<([^<>]*)>')
         full = ""
         m = ""
+        # Findall returns a list of str unless it has more than 1 group then it returns as list of match objects. Match is not counted as a group??
         matches = r.findall(output)
 
         for match in matches:
@@ -29,7 +32,7 @@ def ReplaceValues(input_string, bot_data):
 
             m = match
 
-            r = re.compile('^[^\[\{\(]*')
+            r = re.compile('^[^\\[\\{\\(]*')
             name = r.search(m).group(0)
             
 
@@ -50,7 +53,7 @@ def ReplaceValues(input_string, bot_data):
             index = int(ParseArguments(args, "[", "]")[0])
             item = v.get("Value")[index]
             if item:
-                output = output.replace(full,str(item))
+                output = output.replace(full,item)
 
         elif v.get("Type") == "Dictionary":
             if "(" in args and ")" in args:
@@ -68,4 +71,90 @@ def ReplaceValues(input_string, bot_data):
 
             else:
                 output = output.replace(full,str(v.get("Value")))
+        
     return output
+
+
+def ReplaceValuesRecursive(input_string,bot_data):
+    """
+    bot_data = {"Variables": {"SOURCE":{"Value":{"hellow":"meow"},
+                                        "Type": "Dictionary",
+                                        "Name": "SOURCE"}
+                                        },
+                "GlobalVariables": {}
+                }
+    """
+    toReplace = []
+    r = re.compile('<([^\\[]*)\\[\\*\\]>')
+    matches = r.findall(input_string)
+
+    variables = []
+
+
+    for m in matches:
+        name = m
+
+        variable = bot_data.get("Variables").get(name)
+
+        if not variable:
+            variable = bot_data.get("GlobalVariables").get(name)
+            if not variable:
+                break
+
+        if variable.get("Type") == "List":
+            variables.append(variable)
+            
+    def theEnd(toReplace,bot_data):
+        return [ReplaceValues(replace,bot_data) for replace in toReplace]
+
+
+    if len(variables) > 0:
+        max_index = len(variable.get("Value"))
+        i = 0
+
+        while i < max_index:
+            replaced = input_string
+
+            for variable in variables:
+                variable_Name = variable.get("Name")
+                theList = variable.get("Value")
+                if len(theList) > i:
+                    replaced = replaced.replace(f"<{variable_Name}[*]>", str(theList[i]))
+                else:
+                    replaced = replaced.replace(f"<{variable_Name}[*]>", "NULL")
+
+            toReplace.append(replaced)
+            i += 1
+        return theEnd(toReplace,bot_data)
+
+
+    r = re.compile("<([^\\(]*)\\(\\*\\)>")
+    match = r.match(input_string)
+
+    if match:
+        full = match.group(0)
+        name = match.group(1)
+
+        theDict = bot_data.get("Variables").get(name)
+        if not theDict: theDict = bot_data.get("GlobalVariables").get(name)
+        if not theDict: toReplace.append(input_string)
+        else:
+            for key in theDict.get("Value"):
+                aDict = {key,theDict.get("Value")[key]}
+                toReplace.append(input_string.replace(full,str(aDict)))
+        return theEnd(toReplace, bot_data)
+    
+    r = re.compile("<([^\\{]*)\\{\\*\\}>")
+    match = r.match(input_string)
+
+    if match:
+        full = match.group(0)
+        name = match.group(1)
+
+        theDict = bot_data.get("Variables").get(name)
+        if not theDict: theDict = bot_data.get("GlobalVariables").get(name)
+        if not theDict: toReplace.append(input_string)
+        else:
+            for key in dict(theDict).get("Value"):
+                toReplace.append(input_string.replace(full,str(key)))
+        return theEnd(toReplace, bot_data)
