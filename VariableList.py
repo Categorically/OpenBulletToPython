e293e00ac83bfe77c1bff9313106deb1eb251a1e
Weee <meowtit58@Gmail.com>
2020-08-06 10:56:52 +0100

    VariableList with VariableList methods

---
+++ VariableList.py
@@ -0,0 +1,49 @@
+from CVar import CVar
+from CVar import VarType
+class VariableList:
+
+    # All = [CVar("SOURCE","SOURCE",False,False)]
+    All = []
+    Captures = [v for v in All if v.IsCapture == True and v.Hidden == False]
+    Singles = [v for v in All if v.VarType == VarType().Single]
+    Lists = [v for v in All if v.VarType == VarType().List]
+    Dictionaries = [v for v in All if v.VarType == VarType().Dictionary]
+
+    def VariableList(self):
+        self.All = []
+
+    def VariableListWithList(self,List):
+        self.All = List
+
+    # Renamed from Get
+    def GetWithName(self,name):
+        return next((v for v in self.All if v.Name == name),None)
+
+    # Renamed from Get
+    def GetWithNameAndType(self,name,VarType):
+        return next((v for v in self.All if v.VarType == VarType and v.Name == name),None)
+        
+    def GetSingle(self,name):
+        return self.GetWithNameAndType(name,VarType().Single).Value
+    
+    def GetList(self,name):
+        return self.GetWithNameAndType(name,VarType().List).Value
+
+    def GetDictionary(self,name):
+        return self.GetWithNameAndType(name,VarType().Dictionary).Value
+
+    def VariableExists(self,name):
+        return any([v for v in self.All if v.Name == name])
+
+    # Renamed from VariableExists
+    def VariableExistsWithType(self,name, VarType):
+        return any([v for v in self.All if v.Name == name and v.VarType == VarType])
+    
+    def Set(self,variable):
+        self.Remove(variable.Name)
+
+        self.All.append(variable)
+    def SetNew(self, variable):
+        if self.VariableExists(variable.Name) == False: self.Set(variable)
+    def Remove(self,name):
+        self.All = [v for v in self.All if v.Name != name]
