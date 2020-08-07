c954f65a97413968a30a024103cdd7ffc2abc9e4
Weee <meowtit58@Gmail.com>
2020-08-07 09:07:22 +0100

    SOURCE,ADDRESS,RESPONSECODE methods

--- BotData.py
+++ BotData.py
@@ -2,4 +2,20 @@ from VariableList import VariableList
 
 class BotData:
     Variables = VariableList()
-    
+    class ResponseSource():
+        def get(self):
+            BotData().Variables.GetWithName("SOURCE").Value
+        def set(self,variable):
+            BotData().Variables.Set(variable)
+
+    class Address():
+        def get(self):
+            BotData().Variables.GetWithName("ADDRESS").Value
+        def set(self,variable):
+            BotData().Variables.Set(variable)
+
+    class ResponseCode():
+        def get(self):
+            BotData().Variables.GetWithName("RESPONSECODE").Value
+        def set(self,variable):
+            BotData().Variables.Set(variable)
