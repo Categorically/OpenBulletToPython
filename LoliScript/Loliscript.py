78bd0206e98d2c680c526c0636775f14adca7cd8
Categorically <57874015+Categorically@users.noreply.github.com>
2024-03-15 16:33:33 +0000

    Changed how the script is being run to the way it works in openbulet

--- LoliScript/Loliscript.py
+++ LoliScript/Loliscript.py
@@ -1,6 +1,10 @@
 
-from LoliScript.BlockParser import IsBlock
+from Models.BotData import BotData
+from LoliScript.CommandParser import IsCommand
+from LoliScript.CommandParser import Parse as ParseCommand
+from LoliScript.BlockParser import IsBlock, Parse as ParseBlock
 
+import re
 def CompressedLines(config_text) -> list:
     i = 0
     isScript = False
@@ -20,3 +24,54 @@ def CompressedLines(config_text) -> list:
 
             i += 1
     return compressed
+
+class LoliScript():
+    def __init__(self, script:str) -> None:
+        self.i = 0
+        self.lines: list[str] = []
+        self.script = script
+    def CanProceed(self):
+        return self.i < len(self.lines)
+    
+    def reset(self):
+        self.i = 0
+
+        self.lines = re.split('\r\n|\r|\n', self.script)
+
+    def IsEmptyOrCommentOrDisabled(self, line:str) -> bool:
+        return line.strip() == "" or line.strip().startswith("##") or line.strip().startswith("!")
+
+    def TakeStep(self, data:BotData):
+        
+        if data.status != BotData.BotStatus.NONE and data.status != BotData.BotStatus.SUCCESS and data.status != BotData.BotStatus.CUSTOM:
+            self.i = len(self.lines)
+            return
+
+        currentLine = self.lines[self.i]
+
+        while self.IsEmptyOrCommentOrDisabled(currentLine):
+            self.i += 1
+            currentLine = self.lines[self.i]
+
+        lookahead = 0
+
+        while self.i + 1 + lookahead < len(self.lines):
+            nextline = self.lines[self.i + 1 + lookahead]
+            if nextline.startswith(" ") or nextline.startswith("\t"):
+                currentLine += nextline.lstrip()
+            else:
+                break
+
+            lookahead += 1
+
+        if IsBlock(currentLine):
+            block = None
+
+            block = ParseBlock(currentLine)
+
+            CurrentBlock = block.label
+
+            block.Process(data)
+        elif IsCommand(currentLine):
+            ac7ion = ParseCommand(currentLine, data) # Executes a command
+        self.i += 1 + lookahead
